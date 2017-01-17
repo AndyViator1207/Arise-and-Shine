235f523bcadb571cc8c34dfeb740d8c9a1fcdd04
Andreas H. Wasty <Wasty-Consulting@gmx.de>
2017-01-17 15:28:05 +0100

    arise typo corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def arise
-	render htlm: "Arise and Shine!"
+	render html: "Arise and Shine!"
   end
 end
