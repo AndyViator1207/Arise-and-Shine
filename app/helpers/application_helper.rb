8b14933e974d436737a23e8492521745aea9e8a2
Andreas H. Wasty <Wasty-Consulting@gmx.de>
2017-01-18 17:01:02 +0100

    Page title helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def display_title(page_descriptor="")
+		main_title="ARISE and SHINE!"
+		main_title + (page_descriptor.empty? ? "" : " ... " + page_descriptor + " page")
+	end
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,34 +1,35 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_home_url
     assert_response :success
-	assert_select "title", "Home"
+	assert_select "title", "ARISE and SHINE! ... Home page"
   end
 
   test "should get help" do
     get static_help_url
     assert_response :success
-	assert_select "title", "Help"
+	assert_select "title", "ARISE and SHINE! ... Help page"
   end
 
   test "should get about" do
     get static_about_url
     assert_response :success
-	assert_select "title", "About -> ARISE and SHINE!"
+	assert_select "title", "ARISE and SHINE! ... About page"
   end
   
   test "should get contact" do
     get static_contact_url
     assert_response :success
-	assert_select "title", "Contact"
+	assert_select "title", "ARISE and SHINE! ... Contact page"
   end
 
   test "should get root" do
     get root_url
     assert_response :success
-	assert_select "title", "About -> ARISE and SHINE!"
+	assert_select "title", "ARISE and SHINE! ... About page"
   end
   
 end
