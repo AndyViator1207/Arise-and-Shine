dd65bedb8e7531c7f454cce3f72ec1b3596eddf0
Andreas H. Wasty <Wasty-Consulting@gmx.de>
2017-01-18 03:17:54 +0100

    Static completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'static/home'
-
   get 'static/help'
+  get 'static/about'
+  get 'static/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#arise'
+  root 'static#about'
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -4,11 +4,31 @@ class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_home_url
     assert_response :success
+	assert_select "title", "Home"
   end
 
   test "should get help" do
     get static_help_url
     assert_response :success
+	assert_select "title", "Help"
   end
 
+  test "should get about" do
+    get static_about_url
+    assert_response :success
+	assert_select "title", "About -> ARISE and SHINE!"
+  end
+  
+  test "should get contact" do
+    get static_contact_url
+    assert_response :success
+	assert_select "title", "Contact"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+	assert_select "title", "About -> ARISE and SHINE!"
+  end
+  
 end
